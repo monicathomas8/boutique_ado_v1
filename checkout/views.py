8bdbb6ebcb5ac69aa5a369621f785b82b1b3964a
monicathomas8 <monicathomas8@icloud.com>
2025-03-30 18:20:03 +0100

    stripe update

--- checkout/views.py
+++ checkout/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
-
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
-
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -17,7 +15,7 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-
+    
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
@@ -26,7 +24,7 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
-
+    
     order_form = OrderForm()
 
     if not stripe_public_key:
