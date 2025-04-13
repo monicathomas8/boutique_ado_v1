c4d5124f25532456ab9d6a64d580a709c1369eb5
monicathomas8 <monicathomas8@icloud.com>
2025-04-13 13:48:51 +0100

    preape for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -41,8 +41,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
