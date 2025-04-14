87ac38a8c6a2ddea5dc464472e8198d384014b8a
monicathomas8 <monicathomas8@icloud.com>
2025-04-14 15:06:58 +0100

    deployment part 3

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
     # other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -133,7 +134,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('postgresql://neondb_owner:fxRuC9YK4iaL@ep-sparkling-dawn-a2q33xgg.eu-central-1.aws.neon.tech/thus_stray_prong_583144'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else: 
     DATABASES = {
@@ -183,6 +184,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'bouado'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #static settingand media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #overide static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 
 
 # Stripe
